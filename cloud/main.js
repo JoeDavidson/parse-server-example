0e8ecd6f30eec0dee930ae15068ec2a9d659f049
xvan <LXF990922@outlook.com>
2016-07-29 08:24:53 -0400

    deploy

--- cloud/main.js
+++ cloud/main.js
@@ -1,20 +1,24 @@
 
-Parse.Cloud.define('hello', function(req, res) {
+Parse.Cloud.define('hello', function (req, res) {
   res.success('Hi');
 });
-Parse.Cloud.define("averageStars", function(request, response){
-  var query = new Parse.Query("Review");
-  query.equalTo("movie", request.params.movie);
-  query.find({
-    success: function(results) {
-      var sum = 0;
-      for (var i = 0; i < results.length; ++i) {
-        sum += results[i].get("stars");
-      }
-      response.success(sum / results.length);
-    },
-    error: function() {
-      response.error("movie lookup failed");
+Parse.Cloud.define("sendPush", function (request, response) {
+  Parse.Push.send({
+    channels: ["Me"],
+    data: {
+      alert: "The Giants won against the Mets 2-3."
     }
-  });
+  }, {
+      success: function () {
+        // Push was successful
+        console.log('Success');
+        response.success('Success');
+      },
+      error: function (error) {
+        // Handle error
+        console.log(error)
+        response.error('error');
+      }
+    });
+
 });
